8368ac148653223de63f7d43b7582cd0cd22960c
taigatomita <tiger101837@gmail.com>
2023-05-13 06:51:28 +0900

    Vulnerability fixed.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,14 +2,16 @@
 
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
-
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_own_blog, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
   end
 
-  def show; end
+  def show
+    blog = Blog.find(params[:id])
+    @blog = user_signed_in? && blog.user_id == current_user.id ? blog : Blog.published.find(params[:id])
+  end
 
   def new
     @blog = Blog.new
@@ -43,11 +45,15 @@ class BlogsController < ApplicationController
 
   private
 
-  def set_blog
-    @blog = Blog.find(params[:id])
+  def set_own_blog
+    @blog = current_user.blogs.find(params[:id])
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    if current_user.premium?
+      params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    else
+      params.require(:blog).permit(:title, :content, :secret)
+    end
   end
 end
